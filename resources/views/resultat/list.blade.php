bf319ea3160bb3f33a0177af23e1b26f4432bb50
Rémi Rahir <remirahir25@gmail.com>
2023-10-31 23:08:16 +0100

    logoEquipeComposanteFonctionnel

--- resources/views/resultat/list.blade.php
+++ resources/views/resultat/list.blade.php
@@ -8,10 +8,49 @@
   @csrf
 </form>
 @foreach($resultatList as $resultat)
+<?php 
+$comp=$resultat->idComposante;
+$path;
+if($comp==1){
+    $path="img/science.png";
+}
+elseif($comp==2){
+$path= "img/lsh.png";
+}
+elseif($comp==3){
+    $path= "img/eisine";
+    }elseif($comp==4){
+        $path= "img/sesg.jpeg";
+        }elseif($comp==5){
+            $path= "img/dsp.png";
+            }elseif($comp==6){
+                $path= "img/staps.png";
+                }elseif($comp==7){
+                    $path= "img/medecine.png";
+                    }elseif($comp==8){
+                        $path= "img/cdc.png";
+                        }elseif($comp==9){
+                            $path= "img/img/esireims.png";
+                            }elseif($comp==10){
+                                $path= "img/cdc.png";
+                                }elseif($comp=11){
+                                    $path= "img/pharma.jpeg'";
+                                    }elseif($comp==12){
+                                        $path= "img/institut.png";
+                                        }elseif($comp==13){
+                                            $path= "img/iutrcc.png";
+                                            }elseif($comp==14){
+                                                $path= "img/iuttroyes.png";
+                                                }elseif($comp==15){
+                                                    $path= "img/inspe.png";
+                                                    }else{
+                                                        $path= "img/cdc.png";
+                                                        }
+?>
     <li class="list-group-resultat d-flex align-resultats-center">
       <div class="col-lg-10">
         <span class="">
-        <img src="../{{$resultat->composantes->img}}"  width="50" height="50" />
+        <img src="../{{$path}}"  width="50" height="50" />
         </span>
         <strong>{{$resultat->NomEquipe}}</strong>
         @if(strlen($resultat->Slogan) > 100)
--- resources/views/resultat/show.blade.php
+++ resources/views/resultat/show.blade.php
@@ -2,9 +2,45 @@
 @section('title') Résultat @endsection
 @section('content')
 <?php 
-int comp=$resultat->idComposante;
+$comp=$resultat->idComposante;
+$path;
+if($comp==1){
+    $path="img/science.png";
+}
+elseif($comp==2){
+$path= "img/lsh.png";
+}
+elseif($comp==3){
+    $path= "img/eisine";
+    }elseif($comp==4){
+        $path= "img/sesg.jpeg";
+        }elseif($comp==5){
+            $path= "img/dsp.png";
+            }elseif($comp==6){
+                $path= "img/staps.png";
+                }elseif($comp==7){
+                    $path= "img/medecine.png";
+                    }elseif($comp==8){
+                        $path= "img/cdc.png";
+                        }elseif($comp==9){
+                            $path= "img/img/esireims.png";
+                            }elseif($comp==10){
+                                $path= "img/cdc.png";
+                                }elseif($comp=11){
+                                    $path= "img/pharma.jpeg'";
+                                    }elseif($comp==12){
+                                        $path= "img/institut.png";
+                                        }elseif($comp==13){
+                                            $path= "img/iutrcc.png";
+                                            }elseif($comp==14){
+                                                $path= "img/iuttroyes.png";
+                                                }elseif($comp==15){
+                                                    $path= "img/inspe.png";
+                                                    }else{
+                                                        $path= "img/cdc.png";
+                                                        }
 ?>
-<i> <img src="../../{{$resultat->composantes->img}}" width="50" height="50" />  </i>
+<i> <img src="../../{{$path}}" width="50" height="50" />  </i>
 <strong>{{$resultat->NomEquipe}}</strong>
 {{$resultat->Slogan}}<br/>
 <p></p>
