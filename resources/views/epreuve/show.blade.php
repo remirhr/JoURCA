f96f73a33d5fe5118053c3237473f39985372889
Rémi Rahir <remirahir25@gmail.com>
2023-10-31 19:36:42 +0100

    AddVoirMesResultatForEpreuve1

--- resources/views/epreuve/show.blade.php
+++ resources/views/epreuve/show.blade.php
@@ -7,5 +7,11 @@
 {{$epreuve->description}}<br/>
 <p></p>
 <a href="{{url('epreuve/')}}" class="btn btn-sm btn-primary mb-2 mr-2">Retour à la liste</a>
+@if($epreuve->id == 1)
 <a href="{{url('resultat/')}}" class="btn btn-sm btn-primary mb-2 mr-2">Voir les résultats</a>
+@elseif($epreuve->id == 2)
+
+@else
+
+@endif
 @endsection
